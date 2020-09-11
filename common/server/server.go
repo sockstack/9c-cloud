bf6f7be671a388d7ff098cbc3d166dbaf6a06796
sockstack <85564748@qq.com>
2020-09-11 18:33:17 +0800

    Update server.go

--- common/server/server.go
+++ common/server/server.go
@@ -15,10 +15,6 @@ type server struct {
 func NewServer() *server {
 	svr := grpc.NewServer() //创建gRPC服务
 
-	/**如果有可以注册多个接口服务,结构体要实现对应的接口方法
-	 * user.RegisterLoginServer(s, &server{})
-	 * minMovie.RegisterFbiServer(s, &server{})
-	 */
 	// 在gRPC服务器上注册反射服务
 	reflection.Register(svr)
 	return &server{
@@ -31,7 +27,7 @@ func (s *server) Server() *grpc.Server {
 }
 
 func (s *server) Run(address string) {
-	lis, err := net.Listen("tcp", address)  //监听所有网卡8028端口的TCP连接
+	lis, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Fatalf("监听失败: %v", err)
 	}
